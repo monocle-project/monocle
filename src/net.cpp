5d381a3c8c0aaf1160075afd5848282340cd465f
Erik Mossberg <lingonvecka@gmail.com>
2015-10-21 01:38:26 +0200

    support newer versions of miniupnpc (api change)

--- src/net.cpp
+++ src/net.cpp
@@ -1078,13 +1078,15 @@ void ThreadMapPort()
     const char * minissdpdpath = 0;
     struct UPNPDev * devlist = 0;
     char lanaddr[64];
+    int error = 0;
 
-#ifndef UPNPDISCOVER_SUCCESS
+#if MINIUPNPC_API_VERSION >= 14
+    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 2, &error);
+#elif defined(UPNPDISCOVER_SUCCESS)
     /* miniupnpc 1.5 */
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0);
 #else
     /* miniupnpc 1.6 */
-    int error = 0;
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, &error);
 #endif
 
